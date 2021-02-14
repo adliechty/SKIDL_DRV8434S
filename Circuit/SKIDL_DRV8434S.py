bc32aef5730f95ff118f7b20b19f90ff0c357647
Tony Liechty <tony.liechty@ni.com>
2021-02-14 18:32:42 -0500

    Adding further helper functions

--- Circuit/SKIDL_DRV8434S.py
+++ Circuit/SKIDL_DRV8434S.py
@@ -3,85 +3,94 @@ from SKIDL_Helpers import *
 
 lib_search_paths[KICAD].append('./TI_DRV8434S')
 
-# Create NETs
+#Auto Generated Net Names
 Net('VM')
-Net('AOUT1')
-Net('AOUT2')
-Net('BOUT1')
-Net('BOUT2')
-Net('GND')
 Net('+5VD')
 Net('NFAULT')
 Net('VREF')
-Net('VDD')
-Net('POWER')
-Net('SCLK')
-Net('SDI')
-Net('SDO')
 Net('CS_n')
+Net('VDD')
+Net('SDO')
+Net('SDI')
+Net('SCLK')
+Net('GND')
+Net('CPL')
+Net('CPH')
+Net('VCP')
+Net('AOUT1')
+Net('AOUT2')
+Net('BOUT2')
+Net('BOUT1')
+#End auto Generated Net Names
 
+StepperDriver = InstantiatePart(
+        Part("TI_DRV8434S", "DRV8434SRGER", footprint="StepperLibrary:DRV8434SRGER"),
+        {   'VM'      : 'VM',
+            'AOUT1'   : 'AOUT1',
+            'AOUT2'   : 'AOUT2',
+            'BOUT2'   : 'BOUT2',
+            'BOUT1'   : 'BOUT1',
+            'PAD'     : 'GND',
+            'GND'     : 'GND',
+            'PGND'    : 'GND',
+            'DVDD'    : '+5VD',
+            'NFAULT'  : 'NFAULT',
+            'VREF'    : 'VREF',
+            'NSCS'    : 'CS_n',
+            'SDO'     : 'SDO',
+            'SDI'     : 'SDI',
+            'SCLK'    : 'SCLK',
+            'STEP'    : 'GND',
+            'DIR'     : 'GND',
+            'ENABLE'  : 'VDD',
+            'VSDO'    : 'VDD',
+            'NSLEEP'  : 'VDD',
+            'CPL'     : 'CPL',
+            'CPH'     : 'CPH',
+            'VCP'     : 'VCP'
+        }
+      )
 
-LeftConnector = Part("Connector_Generic", "Conn_01x09", footprint="Connector_PinSocket_2.54mm:PinSocket_1x09_P2.54mm_Vertical")
-RightConnector = Part("Connector_Generic", "Conn_01x09", footprint="Connector_PinSocket_2.54mm:PinSocket_1x09_P2.54mm_Vertical")
-
-StepperDriver = Part("TI_DRV8434S", "DRV8434SRGER", footprint="StepperLibrary:DRV8434SRGER")
-
-#Hook up pins of Stepper Driver to local passives
-StepperDriver['VM'] += Net.get('VM')
-StepperDriver['AOUT1'] += Net.get('AOUT1')
-StepperDriver['AOUT2'] += Net.get('AOUT2')
-StepperDriver['BOUT1'] += Net.get('BOUT1')
-StepperDriver['BOUT2'] += Net.get('BOUT2')
-StepperDriver['GND'] += Net.get('GND')
-StepperDriver['PGND'] += Net.get('GND')
-StepperDriver['PAD'] += Net.get('GND')
-StepperDriver['VCP'] & addPassive('C', '0402', '0.1uF') & StepperDriver['VM']
-StepperDriver['CPH'] & addPassive('C', '0402', '0.1uF') & StepperDriver['CPL']
-StepperDriver['NSLEEP'] += Net.get('VDD')
-StepperDriver['ENABLE'] += Net.get('VDD')
-StepperDriver['VSDO'] += Net.get('VDD')
+addBypassCap('0402', '0.1uF', 'VCP', 'VM')
+addBypassCap('0402', '0.1uF', 'CPH', 'CPL')
 addBypassCaps([['0402', '0.1uF'],
                ['0402', '0.1uF']], 
               'GND', 'VDD')
 
-StepperDriver['DIR'] += Net.get('GND')
-StepperDriver['STEP'] += Net.get('GND')
-StepperDriver['SCLK'] += Net.get('SCLK')
-StepperDriver['SDI'] += Net.get('SDI')
-StepperDriver['SDO'] += Net.get('SDO')
-StepperDriver['NSCS'] += Net.get('CS_n')
-StepperDriver['NFAULT'] += Net.get('NFAULT')
 addPull('0402', '10k', 'NFAULT', '+5VD')
-
-StepperDriver['VREF'] += Net.get('VREF')
-StepperDriver['DVDD'] += Net.get('+5VD')
 addPull     ('0402', '10k', 'VREF', '+5VD')
 addPull     ('0402', '10k', 'VREF', 'GND')
 addBypassCap('0402', '0.1uF', 'GND', 'VREF')
 
+LeftConnector = InstantiatePart(
+        Part("Connector_Generic", "Conn_01x09", footprint="Connector_PinSocket_2.54mm:PinSocket_1x09_P2.54mm_Vertical"),
+          {
+            'Pin_1' : 'GND',
+            'Pin_2' : 'BOUT1',
+            'Pin_3' : 'BOUT2',
+            'Pin_4' : 'AOUT2',
+            'Pin_5' : 'AOUT1',
+            'Pin_6' : 'GND',
+            'Pin_7' : 'VM',
+            'Pin_8' : 'VM',
+            'Pin_9' : 'GND',
+          }
+        )
 
-#Hook up pins of Stepper Driver externally
-LeftConnector[1] += Net.get('GND')
-LeftConnector[2] += Net.get('BOUT1')
-LeftConnector[3] += Net.get('BOUT2')
-LeftConnector[4] += Net.get('AOUT2')
-LeftConnector[5] += Net.get('AOUT1')
-LeftConnector[6] += Net.get('GND')
-LeftConnector[7] += Net.get('POWER')
-LeftConnector[8] += Net.get('POWER')
-LeftConnector[9] += Net.get('GND')
-
-RightConnector[1] += Net.get('VDD')
-RightConnector[2] += Net.get('GND')
-RightConnector[3] += Net.get('SCLK')
-RightConnector[4] += Net.get('GND')
-RightConnector[5] += Net.get('SDI')
-RightConnector[6] += Net.get('GND')
-RightConnector[7] += Net.get('SDO')
-RightConnector[8] += Net.get('GND')
-RightConnector[9] += Net.get('CS_n')
-# Connect the nets and resistors.
-
+RighConnector = InstantiatePart(
+        Part("Connector_Generic", "Conn_01x09", footprint="Connector_PinSocket_2.54mm:PinSocket_1x09_P2.54mm_Vertical"),
+          {
+            'Pin_1' : 'VDD',
+            'Pin_2' : 'GND',
+            'Pin_3' : 'SCLK',
+            'Pin_4' : 'GND',
+            'Pin_5' : 'SDI',
+            'Pin_6' : 'GND',
+            'Pin_7' : 'SDO',
+            'Pin_8' : 'GND',
+            'Pin_9' : 'CS_n',
+          }
+        )
 
 # Or you could do it with a single line of code:
 # vin && r1 && vout && r2 && gnd
--- Circuit/SKIDL_Helpers.py
+++ Circuit/SKIDL_Helpers.py
@@ -24,3 +24,44 @@ def addBypassCaps(sizeValueList, net1, net2):
 def addPull(size, value, net1, net2):
     pull = addPassive('R', size, value)
     Net.get(net1) & pull & Net.get(net2)
+
+def InstantiatePart(part, pinToNetMapping):
+    pins = part.get_pins()
+    missingPinToNetMappings = []
+    newNets = []
+    pinNames = []
+    for pin in pins:
+      if pin.name not in pinNames:
+        pinNames.append(pin.name)
+
+    print("change this to in pins so that we attach this to pins not to a string")
+    for pin in pins:
+      #Check if net name is in the net name mapping
+      if pin.name in pinToNetMapping:
+        NetName = pinToNetMapping[pin.name]
+      else:
+        NetName = pin.name
+        missingPinToNetMappings.append(NetName)
+      #Net already exists
+      if Net.get(NetName):
+        pin += Net.get(NetName)
+        print("Existing net: " + NetName)
+      #Create a new net
+      else:
+        pin += Net(NetName)
+        newNets.append(NetName)
+        print("New net:      " + NetName)
+
+    if missingPinToNetMappings:
+      maxStrLength = max(len(x) for x in missingPinToNetMappings) 
+      print("WARNING:  Add these mapings to the part instantiation, if mapping is 1:1 with pin name to net name:")
+      for pin in missingPinToNetMappings:
+        print('    \'' + pin + '\'' + ' ' * (maxStrLength - len(pin) + 1) + ': \'' + pin + '\',')
+
+    if newNets:
+      print("WARNING:  Add these Net name definitions:")
+      for net in newNets:
+        print('Net(\'' + net + '\')')
+
+    print("Possible FEATURE:  May be good to change this function to enforce you to state pin number as well as name to ensure that mapping is correct or at least to see that mapping in the code")
+    return part
