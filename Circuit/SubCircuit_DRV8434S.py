2c55252e72b81e808bb73ce778e406400c56b643
Tony Liechty <tony.liechty@ni.com>
2021-02-15 08:29:58 -0500

    Adding sub circuit
    Intent is to have a class per sub circuit.  This is a quick and dirty version that does not utilize some of the classes provided by SKIDL, hope to do that in the future.

---
+++ Circuit/SubCircuit_DRV8434S.py
@@ -0,0 +1,97 @@
+from skidl import *
+from SKIDL_Helpers import *
+
+lib_search_paths[KICAD].append('./TI_DRV8434S')
+
+class SubCircuit_DRV8434S:
+    def __init__(self, name, pinsToNets):
+      self.name = name
+      self.DefineRequiredPins()
+      self.pinsToNets = pinsToNets
+      self.CheckAllPinsAssigned()
+      self.DefineSubCircuit()
+
+    #Modify this to define pins of the sub circuit
+    def DefineRequiredPins(self):
+      print("InDefineRequiredPins")
+      self.requiredPins = ['VM',
+                   'AOUT1',
+                   'AOUT2',
+                   'BOUT1',
+                   'BOUT2',
+                   'GND',
+                   'VIO',
+                   'SCLK',
+                   'SDI',
+                   'SDO',
+                   'CS_n'
+                   ]
+    def CreateNet(self, NetName):
+      Net(self.name + "_" + NetName)
+      return self.name + "_" + NetName
+    def GetNetName(self, NetName):
+      return self.name + "_" + NetName
+    #Modify this function for your desired sub circuit
+    def DefineSubCircuit(self):
+      VD     = self.CreateNet('+5VD')
+      NFAULT = self.CreateNet('NFAULT')
+      VREF   = self.CreateNet('VREF')
+      CPL    = self.CreateNet('CPL')
+      CPH    = self.CreateNet('CPH')
+      VCP    = self.CreateNet('VCP')
+      
+      InstantiatePart(
+      Part("TI_DRV8434S", "DRV8434SRGER", footprint="StepperLibrary:DRV8434SRGER"),
+        {   'VM'      : self.pinsToNets['VM'],
+            'AOUT1'   : self.pinsToNets['AOUT1'],
+            'AOUT2'   : self.pinsToNets['AOUT2'],
+            'BOUT2'   : self.pinsToNets['BOUT2'],
+            'BOUT1'   : self.pinsToNets['BOUT1'],
+            'PAD'     : self.pinsToNets['GND'],
+            'GND'     : self.pinsToNets['GND'],
+            'PGND'    : self.pinsToNets['GND'],
+            'DVDD'    : VD,
+            'NFAULT'  : NFAULT,
+            'VREF'    : VREF,
+            'NSCS'    : self.pinsToNets['CS_n'],
+            'SDO'     : self.pinsToNets['SDO'],
+            'SDI'     : self.pinsToNets['SDI'],
+            'SCLK'    : self.pinsToNets['SCLK'],
+            'STEP'    : self.pinsToNets['GND'],
+            'DIR'     : self.pinsToNets['GND'],
+            'ENABLE'  : self.pinsToNets['VIO'],
+            'VSDO'    : self.pinsToNets['VIO'],
+            'NSLEEP'  : self.pinsToNets['VIO'],
+            'CPL'     : CPL,
+            'CPH'     : CPH,
+            'VCP'     : VCP
+        }
+      )
+
+      addBypassCap('0402', '0.1uF', VCP, self.pinsToNets['VM'])
+      addBypassCap('0402', '0.1uF', CPH, CPL)
+
+      addBypassCap('0402', '0.1uF', self.pinsToNets['GND'], self.pinsToNets['VIO']) #one cap for VSDO pin
+      addBypassCap('0402', '0.1uF', self.pinsToNets['GND'], self.pinsToNets['VIO']) #one cap for ENABLE and NSLEEP pins that are close to each other.
+
+      addPull('0402', '10k', NFAULT, VD)
+
+      addPull('0402', '10k', VREF, VD)
+      addPull('0402', '10k', VREF, self.pinsToNets['GND'])
+      addBypassCap('0402', '0.1uF', self.pinsToNets['GND'], VREF)
+
+
+    #Random helper functions.  No need to modify these
+    def CheckAllPinsAssigned(self):
+      missingPinToNetMappings = []
+      for pin in self.requiredPins:
+        if pin not in self.pinsToNets:
+          missingPinToNetMappings.append(pin)
+
+      if missingPinToNetMappings:
+        maxStrLength = max(len(x) for x in missingPinToNetMappings) 
+        print("WARNING:  Add these mapings to the part instantiation, if mapping is 1:1 with pin name to net name:")
+        for pin in missingPinToNetMappings:
+          print('    \'' + pin + '\'' + ' ' * (maxStrLength - len(pin) + 1) + ': \'' + pin + '\',')
+
+              
